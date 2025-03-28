8333fd97a977ec7a4efe9280510bbbd1ead4d39a
Musale Martin <martinmusale@microsoft.com>
2025-03-28 15:42:26 +0300

    Add more project files for .net core library

---
+++ dotnet/tests/Microsoft.Agents.M365Copilot.Core.Tests/TestModels/ServiceModels/TestBodyType.cs
@@ -0,0 +1,28 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+using System.Runtime.Serialization;
+
+namespace Microsoft.Agents.M365Copilot.Core.Tests.TestModels.ServiceModels
+{
+    /// <summary>
+    /// The enum BodyType.
+    /// </summary>
+    public enum TestBodyType
+    {
+
+        /// <summary>
+        /// Text
+        /// </summary>
+        [EnumMember(Value = "text")]
+        Text = 0,
+
+        /// <summary>
+        /// Html
+        /// </summary>
+        [EnumMember(Value = "html")]
+        Html = 1,
+
+    }
+}
