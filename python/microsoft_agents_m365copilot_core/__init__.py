d4d299405e01af336d0f344ad6a96b842110937f
Musale Martin <martinmusale@microsoft.com>
2025-04-16 12:00:54 +0300

    Remove unnecessary imports

--- python/microsoft_agents_m365copilot_core/__init__.py
+++ python/microsoft_agents_m365copilot_core/__init__.py
@@ -1,8 +1,7 @@
 from microsoft_agents_m365copilot_core.src import (
     SDK_VERSION, MicrosoftAgentsM365CopilotClientFactory, BaseMicrosoftAgentsM365CopilotRequestAdapter,
     PageResult, AzureIdentityAuthenticationProvider, FeatureUsageFlag, NationalClouds,
-    APIVersion, UploadResult, UploadSessionDataHolder,
-    MicrosoftAgentsM365CopilotTelemetryHandlerOption, MicrosoftAgentsM365CopilotRequestContext,
+    APIVersion, MicrosoftAgentsM365CopilotTelemetryHandlerOption, MicrosoftAgentsM365CopilotRequestContext,
     AsyncMicrosoftAgentsM365CopilotTransport, MicrosoftAgentsM365CopilotTelemetryHandler
 )
 
@@ -10,7 +9,6 @@ __all__ = [
     "MicrosoftAgentsM365CopilotClientFactory",
     "BaseMicrosoftAgentsM365CopilotRequestAdapter", "PageResult",
     "AzureIdentityAuthenticationProvider", "FeatureUsageFlag", "NationalClouds", "APIVersion",
-    "UploadResult", "UploadSessionDataHolder",
     "MicrosoftAgentsM365CopilotTelemetryHandlerOption", "MicrosoftAgentsM365CopilotRequestContext",
     "AsyncMicrosoftAgentsM365CopilotTransport", "MicrosoftAgentsM365CopilotTelemetryHandler",
     "SDK_VERSION"
