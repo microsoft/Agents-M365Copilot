8d31b92c27e00c5e95eaad8555e23f013f46c17c
Musale Martin <martinmusale@microsoft.com>
2025-05-14 15:37:51 +0300

    Create a packages folder and move all files there

--- python/microsoft_agents_m365copilot_core/tests/authentication/test_azure_identity_authentication_provider.py
+++
@@ -1,8 +0,0 @@
-from azure.identity import EnvironmentCredential
-from kiota_abstractions.authentication import AuthenticationProvider
-
-from microsoft_agents_m365copilot_core import AzureIdentityAuthenticationProvider
-
-
-def test_subclassing():
-    assert issubclass(AzureIdentityAuthenticationProvider, AuthenticationProvider)
