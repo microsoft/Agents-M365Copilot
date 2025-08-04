66029493124e5501c017123f887bf3953817a7b1
Michael Mainer (from Dev Box) <8527305+MIchaelMainer@users.noreply.github.com>
2025-08-04 07:29:28 -0700

    chore: add yapf ignore

--- python/packages/microsoft_agents_m365copilot_core/microsoft_agents_m365copilot_core/__init__.py
+++ python/packages/microsoft_agents_m365copilot_core/microsoft_agents_m365copilot_core/__init__.py
@@ -1,4 +1,5 @@
 from .src import (
+    SDK_VERSION,
     APIVersion,
     AsyncMicrosoftAgentsM365CopilotTransport,
     AzureIdentityAuthenticationProvider,
@@ -9,19 +10,12 @@ from .src import (
     MicrosoftAgentsM365CopilotTelemetryHandler,
     MicrosoftAgentsM365CopilotTelemetryHandlerOption,
     NationalClouds,
-    SDK_VERSION
 )
 
 __all__ = [
-    "APIVersion",
-    "AsyncMicrosoftAgentsM365CopilotTransport",
-    "AzureIdentityAuthenticationProvider",
-    "BaseMicrosoftAgentsM365CopilotRequestAdapter",
-    "FeatureUsageFlag",
-    "MicrosoftAgentsM365CopilotClientFactory",
-    "MicrosoftAgentsM365CopilotRequestContext",
-    "MicrosoftAgentsM365CopilotTelemetryHandler",
-    "MicrosoftAgentsM365CopilotTelemetryHandlerOption",
-    "NationalClouds",
+    "MicrosoftAgentsM365CopilotClientFactory", "BaseMicrosoftAgentsM365CopilotRequestAdapter",
+    "AzureIdentityAuthenticationProvider", "FeatureUsageFlag", "NationalClouds", "APIVersion",
+    "MicrosoftAgentsM365CopilotTelemetryHandlerOption", "MicrosoftAgentsM365CopilotRequestContext",
+    "AsyncMicrosoftAgentsM365CopilotTransport", "MicrosoftAgentsM365CopilotTelemetryHandler",
     "SDK_VERSION"
 ]
