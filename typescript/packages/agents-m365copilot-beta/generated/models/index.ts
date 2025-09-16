4d0476c27732c4b8d41b8c0c2154ab74c1f9c85c
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-09-16 17:05:30 +0000

    feat(generation): update request builders and models for typescript beta
    Update generated files with build 198036

--- typescript/packages/agents-m365copilot-beta/generated/models/index.ts
+++ typescript/packages/agents-m365copilot-beta/generated/models/index.ts
@@ -2780,11 +2780,11 @@ export interface Identity extends AdditionalDataHolder, BackedModel, Parsable {
      */
     backingStoreEnabled?: boolean | null;
     /**
-     * The display name of the identity. For drive items, the display name might not always be available or up to date. For example, if a user changes their display name the API might show the new value in a future response, but the items associated with the user don't show up as changed when using delta.
+     * The display name of the identity. This property is read-only.
      */
     displayName?: string | null;
     /**
-     * Unique identifier for the identity or actor. For example, in the access reviews decisions API, this property might record the id of the principal, that is, the group, user, or application that's subject to review.
+     * The identifier of the identity. This property is read-only.
      */
     id?: string | null;
     /**
