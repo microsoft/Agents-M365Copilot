0a8cc6b6df53575627b6996a9cfa683c7fc48a0c
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-10-07 10:53:04 +0000

    feat(generation): update request builders and models for python beta
    Update generated files with build 199559

--- python/packages/microsoft_agents_m365copilot_beta/microsoft_agents_m365copilot_beta/generated/copilot/communications/realtime_activity_feed/multi_activity_subscriptions/item/get_artifacts/get_artifacts_post_request_body.py
+++ python/packages/microsoft_agents_m365copilot_beta/microsoft_agents_m365copilot_beta/generated/copilot/communications/realtime_activity_feed/multi_activity_subscriptions/item/get_artifacts/get_artifacts_post_request_body.py
@@ -7,52 +7,52 @@ from kiota_abstractions.store import BackedModel, BackingStore, BackingStoreFact
 from typing import Any, Optional, TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
-    from .transcript_speaker import TranscriptSpeaker
+    from .......models.artifact_type import ArtifactType
 
 @dataclass
-class TranscriptPayload(AdditionalDataHolder, BackedModel, Parsable):
+class GetArtifactsPostRequestBody(AdditionalDataHolder, BackedModel, Parsable):
     # Stores model information.
     backing_store: BackingStore = field(default_factory=BackingStoreFactorySingleton(backing_store_factory=None).backing_store_factory.create_backing_store, repr=False)
 
     # Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
     additional_data: dict[str, Any] = field(default_factory=dict)
-    # The audioCaptureDateTime property
-    audio_capture_date_time: Optional[datetime.datetime] = None
-    # The OdataType property
-    odata_type: Optional[str] = None
-    # The speaker property
-    speaker: Optional[TranscriptSpeaker] = None
-    # The spokenLanguage property
-    spoken_language: Optional[str] = None
-    # The text property
-    text: Optional[str] = None
+    # The artifactTypes property
+    artifact_types: Optional[list[ArtifactType]] = None
+    # The maxResults property
+    max_results: Optional[int] = None
+    # The rangeInSec property
+    range_in_sec: Optional[int] = None
+    # The seedDateTime property
+    seed_date_time: Optional[datetime.datetime] = None
+    # The userId property
+    user_id: Optional[str] = None
     
     @staticmethod
-    def create_from_discriminator_value(parse_node: ParseNode) -> TranscriptPayload:
+    def create_from_discriminator_value(parse_node: ParseNode) -> GetArtifactsPostRequestBody:
         """
         Creates a new instance of the appropriate class based on discriminator value
         param parse_node: The parse node to use to read the discriminator value and create the object
-        Returns: TranscriptPayload
+        Returns: GetArtifactsPostRequestBody
         """
         if parse_node is None:
             raise TypeError("parse_node cannot be null.")
-        return TranscriptPayload()
+        return GetArtifactsPostRequestBody()
     
     def get_field_deserializers(self,) -> dict[str, Callable[[ParseNode], None]]:
         """
         The deserialization information for the current model
         Returns: dict[str, Callable[[ParseNode], None]]
         """
-        from .transcript_speaker import TranscriptSpeaker
+        from .......models.artifact_type import ArtifactType
 
-        from .transcript_speaker import TranscriptSpeaker
+        from .......models.artifact_type import ArtifactType
 
         fields: dict[str, Callable[[Any], None]] = {
-            "audioCaptureDateTime": lambda n : setattr(self, 'audio_capture_date_time', n.get_datetime_value()),
-            "@odata.type": lambda n : setattr(self, 'odata_type', n.get_str_value()),
-            "speaker": lambda n : setattr(self, 'speaker', n.get_object_value(TranscriptSpeaker)),
-            "spokenLanguage": lambda n : setattr(self, 'spoken_language', n.get_str_value()),
-            "text": lambda n : setattr(self, 'text', n.get_str_value()),
+            "artifactTypes": lambda n : setattr(self, 'artifact_types', n.get_collection_of_enum_values(ArtifactType)),
+            "maxResults": lambda n : setattr(self, 'max_results', n.get_int_value()),
+            "rangeInSec": lambda n : setattr(self, 'range_in_sec', n.get_int_value()),
+            "seedDateTime": lambda n : setattr(self, 'seed_date_time', n.get_datetime_value()),
+            "userId": lambda n : setattr(self, 'user_id', n.get_str_value()),
         }
         return fields
     
@@ -64,11 +64,11 @@ class TranscriptPayload(AdditionalDataHolder, BackedModel, Parsable):
         """
         if writer is None:
             raise TypeError("writer cannot be null.")
-        writer.write_datetime_value("audioCaptureDateTime", self.audio_capture_date_time)
-        writer.write_str_value("@odata.type", self.odata_type)
-        writer.write_object_value("speaker", self.speaker)
-        writer.write_str_value("spokenLanguage", self.spoken_language)
-        writer.write_str_value("text", self.text)
+        writer.write_collection_of_enum_values("artifactTypes", self.artifact_types)
+        writer.write_int_value("maxResults", self.max_results)
+        writer.write_int_value("rangeInSec", self.range_in_sec)
+        writer.write_datetime_value("seedDateTime", self.seed_date_time)
+        writer.write_str_value("userId", self.user_id)
         writer.write_additional_data_value(self.additional_data)
     
 
