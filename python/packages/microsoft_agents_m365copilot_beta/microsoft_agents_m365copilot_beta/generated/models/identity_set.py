608a3a5756966be4b9823180fd20ad4fbb779091
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-11-04 11:04:49 +0000

    feat(generation): update request builders and models for python beta
    Update generated files with build 202235

--- python/packages/microsoft_agents_m365copilot_beta/microsoft_agents_m365copilot_beta/generated/models/identity_set.py
+++ python/packages/microsoft_agents_m365copilot_beta/microsoft_agents_m365copilot_beta/generated/models/identity_set.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from collections.abc import Callable
 from dataclasses import dataclass, field
 from kiota_abstractions.serialization import AdditionalDataHolder, Parsable, ParseNode, SerializationWriter
-from kiota_abstractions.store import BackedModel, BackingStore, BackingStoreFactorySingleton
 from typing import Any, Optional, TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
@@ -17,12 +16,10 @@ if TYPE_CHECKING:
     from .share_point_identity_set import SharePointIdentitySet
 
 @dataclass
-class IdentitySet(AdditionalDataHolder, BackedModel, Parsable):
-    # Stores model information.
-    backing_store: BackingStore = field(default_factory=BackingStoreFactorySingleton(backing_store_factory=None).backing_store_factory.create_backing_store, repr=False)
-
+class IdentitySet(AdditionalDataHolder, Parsable):
     # Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
     additional_data: dict[str, Any] = field(default_factory=dict)
+
     # Optional. The application associated with this action.
     application: Optional[Identity] = None
     # Optional. The device associated with this action.
--- python/packages/microsoft_agents_m365copilot_beta/microsoft_agents_m365copilot_beta/generated/models/meeting_note.py
+++ python/packages/microsoft_agents_m365copilot_beta/microsoft_agents_m365copilot_beta/generated/models/meeting_note.py
@@ -2,19 +2,16 @@ from __future__ import annotations
 from collections.abc import Callable
 from dataclasses import dataclass, field
 from kiota_abstractions.serialization import AdditionalDataHolder, Parsable, ParseNode, SerializationWriter
-from kiota_abstractions.store import BackedModel, BackingStore, BackingStoreFactorySingleton
 from typing import Any, Optional, TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
     from .meeting_note_subpoint import MeetingNoteSubpoint
 
 @dataclass
-class MeetingNote(AdditionalDataHolder, BackedModel, Parsable):
-    # Stores model information.
-    backing_store: BackingStore = field(default_factory=BackingStoreFactorySingleton(backing_store_factory=None).backing_store_factory.create_backing_store, repr=False)
-
+class MeetingNote(AdditionalDataHolder, Parsable):
     # Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
     additional_data: dict[str, Any] = field(default_factory=dict)
+
     # The OdataType property
     odata_type: Optional[str] = None
     # The subpoints property
