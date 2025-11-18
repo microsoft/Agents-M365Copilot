2bfb92b41917702c24ccd9ba39c5f456cb06b8ee
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-11-18 23:43:08 +0000

    feat(generation): update request builders and models for python beta
    Update generated files with build 203832

--- python/packages/microsoft_agents_m365copilot_beta/microsoft_agents_m365copilot_beta/generated/models/agent_collection_response.py
+++ python/packages/microsoft_agents_m365copilot_beta/microsoft_agents_m365copilot_beta/generated/models/agent_collection_response.py
@@ -5,48 +5,40 @@ from kiota_abstractions.serialization import Parsable, ParseNode, SerializationW
 from typing import Any, Optional, TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
-    from .copilot_admin_catalog import CopilotAdminCatalog
-    from .copilot_admin_setting import CopilotAdminSetting
-    from .entity import Entity
+    from .agent import Agent
+    from .base_collection_pagination_count_response import BaseCollectionPaginationCountResponse
 
-from .entity import Entity
+from .base_collection_pagination_count_response import BaseCollectionPaginationCountResponse
 
 @dataclass
-class CopilotAdmin(Entity, Parsable):
-    # The catalog property
-    catalog: Optional[CopilotAdminCatalog] = None
-    # The OdataType property
-    odata_type: Optional[str] = None
-    # The settings property
-    settings: Optional[CopilotAdminSetting] = None
+class AgentCollectionResponse(BaseCollectionPaginationCountResponse, Parsable):
+    # The value property
+    value: Optional[list[Agent]] = None
     
     @staticmethod
-    def create_from_discriminator_value(parse_node: ParseNode) -> CopilotAdmin:
+    def create_from_discriminator_value(parse_node: ParseNode) -> AgentCollectionResponse:
         """
         Creates a new instance of the appropriate class based on discriminator value
         param parse_node: The parse node to use to read the discriminator value and create the object
-        Returns: CopilotAdmin
+        Returns: AgentCollectionResponse
         """
         if parse_node is None:
             raise TypeError("parse_node cannot be null.")
-        return CopilotAdmin()
+        return AgentCollectionResponse()
     
     def get_field_deserializers(self,) -> dict[str, Callable[[ParseNode], None]]:
         """
         The deserialization information for the current model
         Returns: dict[str, Callable[[ParseNode], None]]
         """
-        from .copilot_admin_catalog import CopilotAdminCatalog
-        from .copilot_admin_setting import CopilotAdminSetting
-        from .entity import Entity
+        from .agent import Agent
+        from .base_collection_pagination_count_response import BaseCollectionPaginationCountResponse
 
-        from .copilot_admin_catalog import CopilotAdminCatalog
-        from .copilot_admin_setting import CopilotAdminSetting
-        from .entity import Entity
+        from .agent import Agent
+        from .base_collection_pagination_count_response import BaseCollectionPaginationCountResponse
 
         fields: dict[str, Callable[[Any], None]] = {
-            "catalog": lambda n : setattr(self, 'catalog', n.get_object_value(CopilotAdminCatalog)),
-            "settings": lambda n : setattr(self, 'settings', n.get_object_value(CopilotAdminSetting)),
+            "value": lambda n : setattr(self, 'value', n.get_collection_of_object_values(Agent)),
         }
         super_fields = super().get_field_deserializers()
         fields.update(super_fields)
@@ -61,7 +53,6 @@ class CopilotAdmin(Entity, Parsable):
         if writer is None:
             raise TypeError("writer cannot be null.")
         super().serialize(writer)
-        writer.write_object_value("catalog", self.catalog)
-        writer.write_object_value("settings", self.settings)
+        writer.write_collection_of_object_values("value", self.value)
     
 
