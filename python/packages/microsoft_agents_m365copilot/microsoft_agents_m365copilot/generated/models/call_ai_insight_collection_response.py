02f1cfa26c1696a8c03120105ac6411a8140a747
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2026-01-06 20:48:55 +0000

    feat(generation): update request builders and models for python v1
    Update generated files with build 207061

--- python/packages/microsoft_agents_m365copilot/microsoft_agents_m365copilot/generated/models/call_ai_insight_collection_response.py
+++ python/packages/microsoft_agents_m365copilot/microsoft_agents_m365copilot/generated/models/call_ai_insight_collection_response.py
@@ -5,48 +5,40 @@ from kiota_abstractions.serialization import Parsable, ParseNode, SerializationW
 from typing import Any, Optional, TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
-    from .ai_interaction_history import AiInteractionHistory
-    from .ai_online_meeting import AiOnlineMeeting
-    from .entity import Entity
+    from .base_collection_pagination_count_response import BaseCollectionPaginationCountResponse
+    from .call_ai_insight import CallAiInsight
 
-from .entity import Entity
+from .base_collection_pagination_count_response import BaseCollectionPaginationCountResponse
 
 @dataclass
-class AiUser(Entity, Parsable):
-    # The interactionHistory property
-    interaction_history: Optional[AiInteractionHistory] = None
-    # The OdataType property
-    odata_type: Optional[str] = None
-    # The onlineMeetings property
-    online_meetings: Optional[list[AiOnlineMeeting]] = None
+class CallAiInsightCollectionResponse(BaseCollectionPaginationCountResponse, Parsable):
+    # The value property
+    value: Optional[list[CallAiInsight]] = None
     
     @staticmethod
-    def create_from_discriminator_value(parse_node: ParseNode) -> AiUser:
+    def create_from_discriminator_value(parse_node: ParseNode) -> CallAiInsightCollectionResponse:
         """
         Creates a new instance of the appropriate class based on discriminator value
         param parse_node: The parse node to use to read the discriminator value and create the object
-        Returns: AiUser
+        Returns: CallAiInsightCollectionResponse
         """
         if parse_node is None:
             raise TypeError("parse_node cannot be null.")
-        return AiUser()
+        return CallAiInsightCollectionResponse()
     
     def get_field_deserializers(self,) -> dict[str, Callable[[ParseNode], None]]:
         """
         The deserialization information for the current model
         Returns: dict[str, Callable[[ParseNode], None]]
         """
-        from .ai_interaction_history import AiInteractionHistory
-        from .ai_online_meeting import AiOnlineMeeting
-        from .entity import Entity
+        from .base_collection_pagination_count_response import BaseCollectionPaginationCountResponse
+        from .call_ai_insight import CallAiInsight
 
-        from .ai_interaction_history import AiInteractionHistory
-        from .ai_online_meeting import AiOnlineMeeting
-        from .entity import Entity
+        from .base_collection_pagination_count_response import BaseCollectionPaginationCountResponse
+        from .call_ai_insight import CallAiInsight
 
         fields: dict[str, Callable[[Any], None]] = {
-            "interactionHistory": lambda n : setattr(self, 'interaction_history', n.get_object_value(AiInteractionHistory)),
-            "onlineMeetings": lambda n : setattr(self, 'online_meetings', n.get_collection_of_object_values(AiOnlineMeeting)),
+            "value": lambda n : setattr(self, 'value', n.get_collection_of_object_values(CallAiInsight)),
         }
         super_fields = super().get_field_deserializers()
         fields.update(super_fields)
@@ -61,7 +53,6 @@ class AiUser(Entity, Parsable):
         if writer is None:
             raise TypeError("writer cannot be null.")
         super().serialize(writer)
-        writer.write_object_value("interactionHistory", self.interaction_history)
-        writer.write_collection_of_object_values("onlineMeetings", self.online_meetings)
+        writer.write_collection_of_object_values("value", self.value)
     
 
