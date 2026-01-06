43ec057ec894d8157c5729b2ae98e3e9f7eb6be7
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2026-01-06 20:56:07 +0000

    feat(generation): update request builders and models for python beta
    Update generated files with build 207061

--- python/packages/microsoft_agents_m365copilot_beta/microsoft_agents_m365copilot_beta/generated/models/copilot_contextual_resources.py
+++ python/packages/microsoft_agents_m365copilot_beta/microsoft_agents_m365copilot_beta/generated/models/copilot_contextual_resources.py
@@ -5,54 +5,50 @@ from kiota_abstractions.serialization import AdditionalDataHolder, Parsable, Par
 from typing import Any, Optional, TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
-    from .copilot_search_resource_metadata_dictionary import CopilotSearchResourceMetadataDictionary
-    from .search_resource_metadata_dictionary import SearchResourceMetadataDictionary
+    from .copilot_file import CopilotFile
+    from .copilot_web_context import CopilotWebContext
 
 @dataclass
-class Dictionaries(AdditionalDataHolder, Parsable):
+class CopilotContextualResources(AdditionalDataHolder, Parsable):
+    """
+    Represents copilot contextual resources parameter.
+    """
     # Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
     additional_data: dict[str, Any] = field(default_factory=dict)
 
+    # The files by URI to be used for the request.
+    files: Optional[list[CopilotFile]] = None
     # The OdataType property
     odata_type: Optional[str] = None
+    # The web context to be used for the request.
+    web_context: Optional[CopilotWebContext] = None
     
     @staticmethod
-    def create_from_discriminator_value(parse_node: ParseNode) -> Dictionaries:
+    def create_from_discriminator_value(parse_node: ParseNode) -> CopilotContextualResources:
         """
         Creates a new instance of the appropriate class based on discriminator value
         param parse_node: The parse node to use to read the discriminator value and create the object
-        Returns: Dictionaries
+        Returns: CopilotContextualResources
         """
         if parse_node is None:
             raise TypeError("parse_node cannot be null.")
-        try:
-            child_node = parse_node.get_child_node("@odata.type")
-            mapping_value = child_node.get_str_value() if child_node else None
-        except AttributeError:
-            mapping_value = None
-        if mapping_value and mapping_value.casefold() == "#microsoft.graph.copilotSearchResourceMetadataDictionary".casefold():
-            from .copilot_search_resource_metadata_dictionary import CopilotSearchResourceMetadataDictionary
-
-            return CopilotSearchResourceMetadataDictionary()
-        if mapping_value and mapping_value.casefold() == "#microsoft.graph.searchResourceMetadataDictionary".casefold():
-            from .search_resource_metadata_dictionary import SearchResourceMetadataDictionary
-
-            return SearchResourceMetadataDictionary()
-        return Dictionaries()
+        return CopilotContextualResources()
     
     def get_field_deserializers(self,) -> dict[str, Callable[[ParseNode], None]]:
         """
         The deserialization information for the current model
         Returns: dict[str, Callable[[ParseNode], None]]
         """
-        from .copilot_search_resource_metadata_dictionary import CopilotSearchResourceMetadataDictionary
-        from .search_resource_metadata_dictionary import SearchResourceMetadataDictionary
+        from .copilot_file import CopilotFile
+        from .copilot_web_context import CopilotWebContext
 
-        from .copilot_search_resource_metadata_dictionary import CopilotSearchResourceMetadataDictionary
-        from .search_resource_metadata_dictionary import SearchResourceMetadataDictionary
+        from .copilot_file import CopilotFile
+        from .copilot_web_context import CopilotWebContext
 
         fields: dict[str, Callable[[Any], None]] = {
+            "files": lambda n : setattr(self, 'files', n.get_collection_of_object_values(CopilotFile)),
             "@odata.type": lambda n : setattr(self, 'odata_type', n.get_str_value()),
+            "webContext": lambda n : setattr(self, 'web_context', n.get_object_value(CopilotWebContext)),
         }
         return fields
     
@@ -64,7 +60,9 @@ class Dictionaries(AdditionalDataHolder, Parsable):
         """
         if writer is None:
             raise TypeError("writer cannot be null.")
+        writer.write_collection_of_object_values("files", self.files)
         writer.write_str_value("@odata.type", self.odata_type)
+        writer.write_object_value("webContext", self.web_context)
         writer.write_additional_data_value(self.additional_data)
     
 
